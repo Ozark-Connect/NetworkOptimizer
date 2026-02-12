dcb49d94efdcbe86ac7d1adcb0fd677fd2baf75d
TJ da Tuna <tjvc42@gmail.com>
2026-02-12 12:43:36 -0600

    Fix zoom buttons (zoomDelta back to 1), disable fitBounds animation
    - zoomDelta: 1 so +/- buttons zoom in normal increments (was 0.25, too granular)
    - zoomSnap: 0.25 kept for fitBounds precision
    - animate: false on fitBounds to prevent animation interference when
      multiple view changes happen in quick succession

--- src/NetworkOptimizer.Web/wwwroot/js/floorPlanEditor.js
+++ src/NetworkOptimizer.Web/wwwroot/js/floorPlanEditor.js
@@ -89,7 +89,7 @@ window.fpEditor = {
             var container = document.getElementById(containerId);
             if (!container) { setTimeout(init, 100); return; }
 
-            var m = L.map(containerId, { center: [centerLat, centerLng], zoom: zoom, zoomControl: true, maxZoom: 24, zoomSnap: 0.25, zoomDelta: 0.25, wheelPxPerZoomLevel: 120 });
+            var m = L.map(containerId, { center: [centerLat, centerLng], zoom: zoom, zoomControl: true, maxZoom: 24, zoomSnap: 0.25, zoomDelta: 1, wheelPxPerZoomLevel: 120 });
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 24, maxNativeZoom: 19, attribution: 'OpenStreetMap'
             }).addTo(m);
@@ -233,7 +233,7 @@ window.fpEditor = {
             var lngSpanM = (neLng - swLng) * 111320 * Math.cos((swLat + neLat) / 2 * Math.PI / 180);
             var sz = this._map.getSize();
             console.log('fitBounds:', { widthM: lngSpanM.toFixed(1), heightM: latSpanM.toFixed(1), viewportPx: sz.x + 'x' + sz.y, curZoom: this._map.getZoom() });
-            this._map.fitBounds(bounds, { padding: [0, 0], maxZoom: 24 });
+            this._map.fitBounds(bounds, { padding: [0, 0], maxZoom: 24, animate: false });
             console.log('fitBounds result:', { zoom: this._map.getZoom(), center: this._map.getCenter() });
         }
     },
