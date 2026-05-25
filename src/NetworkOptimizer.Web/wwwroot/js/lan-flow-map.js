b86e54e79a5c21b4f72a6c46d17474daf2344cbe
TJ da Tuna <tjvc42@gmail.com>
2026-05-24 20:47:03 -0500

    3D map: speed test pill + 0.5 below cloud

--- src/NetworkOptimizer.Web/wwwroot/js/lan-flow-map.js
+++ src/NetworkOptimizer.Web/wwwroot/js/lan-flow-map.js
@@ -1987,7 +1987,7 @@ export class LanFlowMap {
             const group = this._cloudMeshes.get(`cloud-access-${wanIface}`);
             if (!group) { pill.classList.remove('is-visible'); continue; }
             tmp.setFromMatrixPosition(group.matrixWorld);
-            tmp.y -= NODE_RADIUS.cloud;
+            tmp.y -= NODE_RADIUS.cloud + 0.5;
             const dist = tmp.distanceTo(camPos);
             tmp.project(this.camera);
             if (tmp.z > 1) { pill.classList.remove('is-visible'); continue; }
