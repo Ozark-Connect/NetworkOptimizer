97bd133ed75fa16eb0263a133b38b3eebd880fc6
TJ da Tuna <tjvc42@gmail.com>
2026-06-06 14:19:50 -0500

    Restore axis padding on ONT/SFP power charts - merge pattern now preserves tooltip formatter

--- src/NetworkOptimizer.Web/wwwroot/js/sfp-charts.js
+++ src/NetworkOptimizer.Web/wwwroot/js/sfp-charts.js
@@ -303,7 +303,8 @@ export async function mount(elId) {
     if (tempChart) { tempChart.destroy(); tempChart = null; }
 
     powerChart = new ApexCharts(powerEl, {
-        ...baseOpts(200, 'dBm', v => v != null ? v.toFixed(1) + ' dBm' : ''),
+        ...baseOpts(200, 'dBm', v => v != null ? v.toFixed(1) + ' dBm' : '', {
+            yaxis: { min: v => Math.floor(v - 2), max: v => Math.ceil(v + 2) } }),
         series: [], colors: PALETTE,
     });
     tempChart = new ApexCharts(tempEl, {
